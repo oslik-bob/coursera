a3f425230feb6fbba019412ddb6aa2d356a2b168
oslik-bob <oslik_bob@mail.ru>
2021-12-09 23:11:47 +0300

    hom

--- serv_n6_v1.py
+++ serv_n6_v1.py
@@ -1,6 +1,61 @@
-import asyncio
-import process_data
+# =============================================================================
+# import asyncio
+# host=input('host')
+# port=input("port")
+#  
+# 
+# 
+# 
+# def run_server(host,port):
+#     
+#     loop = asyncio.get_event_loop()
+#     coro = loop.create_server(ClientServerProtocol,host, int(port))
+#     
+#     server = loop.run_until_complete(coro)
+#     try:
+#         loop.run_forever()
+#     except KeyboardInterrupt:
+#         server.close()
+#     
+#     server.close()
+#     loop.run_until_complete(server.wait_closed())
+#     loop.close()
+#         
+# 
+# 
+# class ClientServerProtocol(asyncio.Protocol):
+#     def connection_made(self, transport):
+#         self.transport = transport
+# 
+#     def data_received(self, data):
+#         resp = process_data(data.decode())
+#         self.transport.write(resp.encode())
+# 
+# if __name__=='__main__':
+#     run_server(host,port)
+# =============================================================================
 
+import asyncio
+import logging
+import concurrent.futures
+
+def run_server(host, port):
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(
+        ClientServerProtocol,
+        host, port)
+
+    server = loop.run_until_complete(coro)
+    #server.
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
 
 
 class ClientServerProtocol(asyncio.Protocol):
@@ -8,23 +63,13 @@ class ClientServerProtocol(asyncio.Protocol):
         self.transport = transport
 
     def data_received(self, data):
-        resp = process_data(data.decode())
+        resp = self.process_data(data.decode())
         self.transport.write(resp.encode())
 
+    def process_data(self, data):
+        print (data)
+        pass
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
 
-loop = asyncio.get_event_loop()
-coro = loop.create_server(
-    ClientServerProtocol,
-    '127.0.0.1', 8181
-)
-
-server = loop.run_until_complete(coro)
-
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    server.close()
-
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
+    run_server("127.0.0.1",8081)
